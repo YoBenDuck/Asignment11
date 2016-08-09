1b3854cf555947e92104c1ef226723a4ce104077
Ben <YoBenDuck@gmail.com>
2016-08-09 16:31:39 -0700

    upload assignment11

---
+++ README.md
@@ -0,0 +1,9 @@
+# COMM644 
+### FEWD 2 Assignment 11  
+### Benjamin Espinoza  
+
+# Assignment 11  
+### Part 1 - Using the jQuery .load() Shorthand Method     
+### Part 2 - Using the jQuery .get() Shorthand Method  
+### Part 3 - Using the jQuery .getJSON() Shorthand Method   
+### Part 4 - Using the jQuery .ajax() Shorthand Method
---
+++ part3/json.js
@@ -0,0 +1,11 @@
+
+{"myCarCollection":[
+    {"make":"Chevy", "model":"Bel Air", "year":"1955", "location":"Garage 10"},
+    {"make":"Chevy", "model":"Nova SS", "year":"1967", "location":"Garage 20"},
+    {"make":"Chevy", "model":"Camaro Yenko SC 427", "year":"1969", "location":"Garage 20"},
+    {"make":"Chevy", "model":"C10", "year":"1970", "location":"Garage 30"},
+    {"make":"Land Rover", "model":"Defender Overfinch", "year":"2015", "location":"Garage 30"},
+    {"make":"International", "model":"Scout II", "year":"1977", "location":"Garage 30"}
+    
+]}
+
